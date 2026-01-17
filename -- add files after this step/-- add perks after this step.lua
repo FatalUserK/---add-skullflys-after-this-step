290ad980b3814d2e4a21a192c9220128236bdc7d
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-01-17 03:06:46 +0000

    added skullfly radar sprites (thx spoop!)

--- -- add files after this step/-- add perks after this step.lua
+++ -- add files after this step/-- add perks after this step.lua
@@ -2,8 +2,8 @@ table.insert(perk_list, {
 	id = "SCAN_SKULLFLYS_AFTER_THIS_STEP",
 	ui_name = "$add_skullfly_radar_name_after_this_step",
 	ui_description = "$add_skullfly_radar_desc_after_this_step",
-	ui_icon = "data/ui_gfx/perk_icons/radar_enemy.png",
-	perk_icon = "data/items_gfx/perks/radar_enemy.png",
+	ui_icon = "mods/-- add skullflys after this step/-- add files after this step/-- add skullfly radar after this step/-- add icon for this perk.png",
+	perk_icon = "mods/-- add skullflys after this step/-- add files after this step/-- add skullfly radar after this step/-- add sprite for this perk.png",
 	stackable = false,
 	func = function(perk, taker, perk_name, times_taken)
 		EntityAddComponent2(taker, "LuaComponent", {
